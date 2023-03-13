df793dfdd91c66df889fbae04db2884801cf4f81
samih111111 <smioo111111>
2023-03-13 13:10:17 -0700

    updated

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        t = len(sentence), None
-        return t
-    t = len(sentence), sentence[0]
-    return 
+    if sentence != '':
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
+ 
